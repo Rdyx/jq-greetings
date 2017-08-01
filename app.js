67f6be2f37e00e1d8a22705a6730416631b4ad86
Rdyx <tiranico.pro@gmail.com>
2017-08-01 16:06:43 +0200

    finish

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('have fun !');
+
+
+$("input").change(function() {
+    var prez = {
+        first : $("#first_name").val(), 
+        last : $("#last_name").val(), 
+        city : $("#city").val(),
+    };
+    $("#username").html(prez.first + " " + prez.last + " de " + prez.city);
+});
